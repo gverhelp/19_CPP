322fb9aab4692b3576ce4b2cf5d2b53682b24407
gverhelp <garreth.verhelpen@gmail.com>
2021-05-27 20:07:14 +0200

    ex00

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -1,4 +1,19 @@
-int main()
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+
+int main(int argc, char **argv)
 {
-    
+    if (argc == 1)
+    {
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+        return (0);
+    }
+    for (int a = 1; a < argc; a++)
+    {
+        for (int b=0; b < (int)strlen(argv[a]); b++)
+            putchar(toupper(argv[a][b]));
+    }
+    putchar('\n');
+    return (0);
 }
