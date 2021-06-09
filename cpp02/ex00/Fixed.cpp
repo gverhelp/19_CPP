0223fddb5aa705093082974cb2ae353bd1baa575
gverhelp <garreth.verhelpen@gmail.com>
2021-06-10 00:35:10 +0200

    cpp02 : ex00 done

--- cpp02/ex00/Fixed.cpp
+++ cpp02/ex00/Fixed.cpp
@@ -7,26 +7,26 @@ Fixed::Fixed()
     pFixe = 0;
 }
 
-Fixed::Fixed(Fixed const& copy)
+Fixed::Fixed(Fixed const &copy)
 {
     std::cout << "Copy constructor called" << std::endl;
     *this = copy;
 }
 
+Fixed& Fixed::operator=(const Fixed &copy)
+{
+    std::cout << "Assignation  operator called" << std::endl;
+    if (this != &copy)
+    {
+        pFixe = copy.pFixe;
+    }
+    pFixe = copy.getRawBits();
+    return (*this);
+}
+
 Fixed::~Fixed()
 {
     std::cout << "Destructor called" << std::endl;
-}
-
-Fixed& Fixed::operator=(Fixed const& copy)
-{
-    std::cout << "Assignation operator called" << std::endl;
-    if (this != &copy)
-    {
-
-    }
-    pFixe = copy.getRawBits();
-    return *this;
 }
 
 void Fixed::setRawBits(int const raw)
--- cpp02/ex00/Fixed.hpp
+++ cpp02/ex00/Fixed.hpp
@@ -5,16 +5,16 @@ class Fixed
 {
     public:
         Fixed();
-        Fixed(Fixed const& copy);
+        Fixed(const Fixed &copy);
+        Fixed& operator=(Fixed const &copy);
         ~Fixed();
-        Fixed& operator=(Fixed const& copy);
 
         void setRawBits(int const raw);
         int getRawBits() const;
 
     private:
         int pFixe;
-        const static int bits = 8;
+        static const int bits = 8;
 };
 
 #endif
